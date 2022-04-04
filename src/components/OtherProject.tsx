d2a44aaebc945694d58a398db59926e044c140ef
Mariana-Usuga <Mariana-Usuga>
2022-04-03 19:33:50 -0500

    better cards

--- src/components/OtherProject.tsx
+++ src/components/OtherProject.tsx
@@ -2,6 +2,8 @@
 /* eslint-disable react/no-unused-prop-types */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { FC } from 'react';
+import { FaFolder, FaGithub } from 'react-icons/fa';
+import { FiExternalLink } from 'react-icons/fi';
 
 interface Project {
   id?:number
@@ -12,10 +14,15 @@ interface Project {
 const OtherProject: FC<Project> = ({ title, children }) => {
   console.log('worki');
   return (
-    <div className="col d-flex justify-content-center">
-      <div className="p-3 border bg-light">
+    <div className="col-md-6 col-lg-4 d-flex justify-content-center border-0">
+      <div className="">
         <div className="card" style={{ width: '20rem' }}>
-          <div className="card-body">
+          <div className="card-body bg-info text-success">
+            <ul>
+              <li><FaFolder /></li>
+              <li><FaGithub /></li>
+              <li><FiExternalLink /></li>
+            </ul>
             <h5 className="card-title">{title}</h5>
             <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
             <p className="card-text">{children}</p>
