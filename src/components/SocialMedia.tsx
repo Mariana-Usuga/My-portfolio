e7ffa401d739e087a43f392ea156d6ac633b5473
Mariana-Usuga <Mariana-Usuga>
2022-04-11 15:21:40 -0500

    better styles

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,27 +1,24 @@
 import { FaLinkedinIn, FaInstagram } from 'react-icons/fa';
 import { GoMarkGithub } from 'react-icons/go';
 
-const SocialMedia = () => {
-  console.log('working');
-  return (
-    <div className="text-success">
-      <ul
-        className="d-flex nose-3"
-      >
-        <li className="align-items-end me-4 link">
-          <FaLinkedinIn
-            style={{ fontSize: '25px' }}
-          />
-        </li>
-        <li className="align-items-end me-4 link">
-          <GoMarkGithub style={{ fontSize: '25px' }} />
-        </li>
-        <li className="align-items-end me-4 link">
-          <FaInstagram style={{ fontSize: '25px' }} />
-        </li>
-      </ul>
-    </div>
-  );
-};
+const SocialMedia = () => (
+  <div className="text-success">
+    <ul
+      className="d-flex nose-3"
+    >
+      <li className="align-items-end me-4 link">
+        <FaLinkedinIn
+          style={{ fontSize: '25px' }}
+        />
+      </li>
+      <li className="align-items-end me-4 link">
+        <GoMarkGithub style={{ fontSize: '25px' }} />
+      </li>
+      <li className="align-items-end me-4 link">
+        <FaInstagram style={{ fontSize: '25px' }} />
+      </li>
+    </ul>
+  </div>
+);
 
 export default SocialMedia;
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -14,31 +14,28 @@ import Projects from '../components/Projects';
 // import WorkPlace from '../components/WorkPlace';
 // import Example from '../components/example';
 
-const Portfolio = () => {
-  console.log('working');
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-1 order-2 order-sm-2 order-md-1">
-          <SocialMedia />
-        </div>
-        <div className="col-md-10 order-1 order-sm-1 order-md-2">
-          <NavBar />
-          <Main />
-          <AboutMe />
-          <WhereIveWorked />
-          <Projects />
-          <OtherProject />
-          <GetInTouch />
-        </div>
-        <div className="col-md-1 order-3 order-sm-3 order-md-3">
-          <Email />
-        </div>
+const Portfolio = () => (
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col-md-1 order-2 order-sm-2 order-md-1">
+        <SocialMedia />
       </div>
-      <StyledCredit />
-      {/* <Example /> */}
+      <div className="col-md-10 order-1 order-sm-1 order-md-2">
+        <NavBar />
+        <Main />
+        <AboutMe />
+        <WhereIveWorked />
+        <Projects />
+        <OtherProject />
+        <GetInTouch />
+      </div>
+      <div className="col-md-1 order-3 order-sm-3 order-md-3">
+        <Email />
+      </div>
     </div>
-  );
-};
+    <StyledCredit />
+    {/* <Example /> */}
+  </div>
+);
 
 export default Portfolio;
